efd6b9f13f9b04665730259cd188512b075bd5e0
divya-1604 <manchandadivya04@gmail.com>
2026-01-31 17:06:33 +0530

    product part start

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,8 +7,18 @@ exports.createProduct = async (req, res) => {
       createdBy: req.user._id,
     });
     const createdProduct = await product.save();
-    res.status(201).json({ message: "product created successfully" });
+    res
+      .status(201)
+      .json({ message: "product created successfully", createdProduct });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find()
+    .populate("category", "name")
+    .populate("createdBy", "name email");
+  res.json(products);
+};
+
+exports.getProductsBySlug = async (req, res) => {};
